17da38bc35220e1c5fdba57977edfed5fe4f4b10
Andre Vitor Granemann <andrevitorgranemann@Andres-MacBook-Air.local>
2022-12-14 19:06:17 -0300

    subindo modulo10

--- modulo10/views.py
+++ modulo10/views.py
@@ -1,63 +1,28 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request, redirect, session, flash, url_for
+from main import app, db
 from models import Jogos, Usuarios
-
-
-
-
-#============================================================================================================================
-#variavel de referencia ao flask 
-app = Flask(__name__)
-
-# encriptar o passwords do usuário
-app.secret_key = 'moredevs'
-
-#string conexao
-app.config['SQLALCHEMY_DATABASE_URI'] = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-    SGBD = 'postgresql',
-    usuario = "teste",
-    senha = "123456",
-    servidor = "localhost",
-    database = "postgres"
-)
-
-#================================================================================
-db = SQLAlchemy(app)
-
-
-#=========================================================================
-lista = Jogos.query.order_by(Jogos.id)
 
 
 @app.route('/')
 def index():
-
     lista = Jogos.query.order_by(Jogos.id)
-
-    #render template acessa nosso html, variavel titulo recebendo valor e sendo acessada via html.
     return render_template("lista.html", titulo = 'jogos', jogos = lista)
 
 
 
-#return redirect('/login')
-@app.route('/novo2')
+
+@app.route('/novo')
 def novo():
-
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        
-        #recurso querystring
-        return redirect(url_for('login2', proximo= url_for('novo2')))
+        return redirect(url_for('login', proximo= url_for('novo')))
+    return render_template('novo.html', titulo='criar Jogo')
 
-    return render_template('novo.html', titulo='Novo Jogo')
-
-
-
-
-@app.route('/criar2', methods=['POST',])
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
+
     #variavel nova recebendo classe jogo e filtrando pelo nome
     jogo = Jogos.query.filter_by(nome=nome).first()
     # if condicional recebendo a variavel caso exista jogos cadastrados 
@@ -75,17 +40,36 @@ def criar():
     return redirect(url_for('index'))
 
 
-@app.route('/login2')
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proximo= url_for('editar')))
+    #fazer uma query do banco
+    jogo = Jogos.query.filter_by(id=id).first()
+    return render_template('editar.html', titulo= 'Editar Jogo', jogo = jogo)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    pass
+
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('voce foi Desconectado')
+
+    return redirect(url_for('login'))
+
+@app.route('/login')
 def login():
 
         proximo = request.args.get('proximo')
 
         return render_template('login.html', proximo=proximo)
 
-
-
-
-@app.route('/autenticar2', methods=['POST',])
+@app.route('/autenticar', methods=['POST',])
 def autenticar():
 
     usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
@@ -108,16 +92,3 @@ def autenticar():
         #dinamizando url
 
         return redirect(url_for('login'))
-
-
-@app.route('/logout2')
-def logout():
-    session['usuario_logado'] = None
-    flash('voce foi Desconectado')
-
-    return redirect(url_for('login'))
-
-
-
-
-app.run(debug=True)
